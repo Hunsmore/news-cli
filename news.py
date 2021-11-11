567fcb22e5a4faddb60ba2d48a79379e0b2901d3
hetianfeng <hetianfeng@meituan.com>
2021-11-11 14:15:15 +0800

    cache paragraphs

--- news.py
+++ news.py
@@ -6,15 +6,20 @@ import time
 from bs4 import BeautifulSoup
 from pathlib import Path
 
-def run(cmd, lang, additional):      
+def run(cmd, lang, additional):    
+
+    cache_path = '/tmp/news-cache/'
+    if not Path(cache_path).exists():
+        Path(cache_path).mkdir()
+
     if cmd == 'list':
         links = []
         titles = []
 
         ts = time.time()
         ts = ts // 1800 * 1800
-        key = 'news-cache-'+lang+'-'+str(ts)
-        path = Path('/tmp/'+key)
+        key = lang+'-'+str(ts)
+        path = Path(cache_path+key)
 
         if path.exists():
             file = path.open(mode='r')
@@ -134,102 +139,121 @@ def run(cmd, lang, additional):
 
     if cmd == 'show':
         url = additional
-
         tit = ''
         pargs = []
 
-        if lang == 'nhk' or lang == 'nhk-more':
-            res = requests.get('https://www3.nhk.or.jp/news/' + url)
-            res.encoding = 'utf-8'
-            soup = BeautifulSoup(res.text, 'html.parser')
-            title = soup.find('h1', {'content--title'}).find('span')
-            tit = title.get_text().strip()
-            sections = soup.find_all('section', {'class': 'content--detail-main'})
-            for section in sections:
-                for paragraph in section.find_all('p'):
-                    text = paragraph.get_text('\n').strip()
-                    if text is not None and text != '':
-                        pargs.append(text)
-        elif lang == 'onu-es' or lang == 'onu-fr' or lang == 'onu-ru' or lang == 'onu-pt' or lang == 'onu-sw':
-            res = requests.get('https://news.un.org' + url)
-            soup = BeautifulSoup(res.text, 'html.parser')
-            title = soup.find('h1', {'class': 'story-title'})
-            tit = title.get_text().strip()
-            paragraphs = soup.find('div', {'class': 'field-type-paragraphs'}).find_all('p')
-            for paragraph in paragraphs:
-                text = paragraph.get_text().strip()
-                if text is not None:
-                    pargs.append(text)
-        elif lang == 'rtve':
-            res = requests.get(url)
-            soup = BeautifulSoup(res.text, 'html.parser')
-            title = soup.find('span', {'class': 'maintitle'})
-            tit = title.get_text().strip()
-            paragraphs = soup.find('div', {'class': 'mainContent hid_email'}).find_all('p')
-            for paragraph in paragraphs:
-                text = paragraph.get_text().strip()
-                if text is not None and text != '':
-                    pargs.append(text)
-        elif lang == 'fr24' or lang == 'fr24-es':
-            res = requests.get('https://www.france24.com'+url, headers=headers)
-            soup = BeautifulSoup(res.text, 'html.parser')
-            title = soup.find('h1', {'class': 't-content__title a-page-title'})
-            tit = title.get_text().strip()
-            brief = soup.find('p', {'class': 't-content__chapo'})
-            pargs.append(brief)
-            paragraphs = soup.find('div', {'class': 't-content__body'}).find_all('p')
-            for paragraph in paragraphs:
-                text = paragraph.get_text().strip()
-                if text is not None and text != '':
-                    pargs.append(text)
-        elif lang == 'detik':
-            res = requests.get(url)
-            soup = BeautifulSoup(res.text, 'html.parser')
-            title = soup.find('h1', {'class': 'detail__title'})
-            tit = title.get_text().strip()
-            pages = soup.find('div', {'class': 'detail__anchor'}).find_all('a')
-            for page in pages:
-                href = page.get('href')
-                res = requests.get(url)
+        key = lang + '-' + url.replace('/','.')
+        path = Path(cache_path+key)
+
+        if path.exists():
+            file = path.open(mode='r')
+            js = file.read()
+            file.close()
+            rs = json.loads(js)
+            tit = rs['tit']
+            pargs = rs['pargs']
+        else:
+            if lang == 'nhk' or lang == 'nhk-more':
+                res = requests.get('https://www3.nhk.or.jp/news/' + url)
+                res.encoding = 'utf-8'
                 soup = BeautifulSoup(res.text, 'html.parser')
-                paragraphs = soup.find('div', {'class': 'detail__body-text itp_bodycontent'}).find_all('p')
+                title = soup.find('h1', {'content--title'}).find('span')
+                tit = title.get_text().strip()
+                sections = soup.find_all('section', {'class': 'content--detail-main'})
+                for section in sections:
+                    for paragraph in section.find_all('p'):
+                        text = paragraph.get_text('\n').strip()
+                        if text is not None and text != '':
+                            pargs.append(text)
+            elif lang == 'onu-es' or lang == 'onu-fr' or lang == 'onu-ru' or lang == 'onu-pt' or lang == 'onu-sw':
+                res = requests.get('https://news.un.org' + url)
+                soup = BeautifulSoup(res.text, 'html.parser')
+                title = soup.find('h1', {'class': 'story-title'})
+                tit = title.get_text().strip()
+                paragraphs = soup.find('div', {'class': 'field-type-paragraphs'}).find_all('p')
                 for paragraph in paragraphs:
                     text = paragraph.get_text().strip()
                     if text is not None:
                         pargs.append(text)
-        elif lang == 'bh-my':
-            es = requests.get('https://www.bharian.com.my'+url)
-            res.encoding = 'utf-8'
-            soup = BeautifulSoup(res.text, 'html.parser')
-            article = soup.find('article-component')
-            article_json = json.loads(article.get(':article'))
-            tit = article_json['title']
-            soup = BeautifulSoup(article_json['body'], 'html.parser')
-            paragraphs = soup.find_all('p')
-            for paragraph in paragraphs:
-                text = paragraph.get_text().strip()
-                if text is not None and text != '':
-                    pargs.append(text)
-        elif lang == 'bh-sg':
-            res = requests.get('https://www.beritaharian.sg'+url)
-            soup = BeautifulSoup(res.text, 'html.parser')
-            title = soup.find('h1', {'class': 'article-headline mb-4'})
-            tit = title.get_text().strip()
-            paragraphs = soup.find('div', {'class': 'odd field-item article'}).find_all('p')
-            for paragraph in paragraphs:
-                text = paragraph.get_text().strip()
-                if text is not None and text != '':
-                    pargs.append(text)
-        elif lang == 'milenio':
-            res = requests.get('https://www.milenio.com'+url)
-            soup = BeautifulSoup(res.text, 'html.parser')
-            title = soup.find('div', {'class': 'bg-content'})
-            tit = title.find('span').get_text()
-            paragraphs = soup.find('div', {'id': 'content-body'}).find_all(re.compile('^((p)|(blockquote)|(h2)|(li))$'))
-            for paragraph in paragraphs:
-                text = paragraph.get_text().strip()
-                if text is not None and len(text)>0:
-                    pargs.append(text)
+            elif lang == 'rtve':
+                res = requests.get(url)
+                soup = BeautifulSoup(res.text, 'html.parser')
+                title = soup.find('span', {'class': 'maintitle'})
+                tit = title.get_text().strip()
+                paragraphs = soup.find('div', {'class': 'mainContent hid_email'}).find_all('p')
+                for paragraph in paragraphs:
+                    text = paragraph.get_text().strip()
+                    if text is not None and text != '':
+                        pargs.append(text)
+            elif lang == 'fr24' or lang == 'fr24-es':
+                res = requests.get('https://www.france24.com'+url, headers=headers)
+                soup = BeautifulSoup(res.text, 'html.parser')
+                title = soup.find('h1', {'class': 't-content__title a-page-title'})
+                tit = title.get_text().strip()
+                brief = soup.find('p', {'class': 't-content__chapo'})
+                pargs.append(brief)
+                paragraphs = soup.find('div', {'class': 't-content__body'}).find_all('p')
+                for paragraph in paragraphs:
+                    text = paragraph.get_text().strip()
+                    if text is not None and text != '':
+                        pargs.append(text)
+            elif lang == 'detik':
+                res = requests.get(url)
+                soup = BeautifulSoup(res.text, 'html.parser')
+                title = soup.find('h1', {'class': 'detail__title'})
+                tit = title.get_text().strip()
+                pages = soup.find('div', {'class': 'detail__anchor'}).find_all('a')
+                for page in pages:
+                    href = page.get('href')
+                    res = requests.get(url)
+                    soup = BeautifulSoup(res.text, 'html.parser')
+                    paragraphs = soup.find('div', {'class': 'detail__body-text itp_bodycontent'}).find_all('p')
+                    for paragraph in paragraphs:
+                        text = paragraph.get_text().strip()
+                        if text is not None:
+                            pargs.append(text)
+            elif lang == 'bh-my':
+                es = requests.get('https://www.bharian.com.my'+url)
+                res.encoding = 'utf-8'
+                soup = BeautifulSoup(res.text, 'html.parser')
+                article = soup.find('article-component')
+                article_json = json.loads(article.get(':article'))
+                tit = article_json['title']
+                soup = BeautifulSoup(article_json['body'], 'html.parser')
+                paragraphs = soup.find_all('p')
+                for paragraph in paragraphs:
+                    text = paragraph.get_text().strip()
+                    if text is not None and text != '':
+                        pargs.append(text)
+            elif lang == 'bh-sg':
+                res = requests.get('https://www.beritaharian.sg'+url)
+                soup = BeautifulSoup(res.text, 'html.parser')
+                title = soup.find('h1', {'class': 'article-headline mb-4'})
+                tit = title.get_text().strip()
+                paragraphs = soup.find('div', {'class': 'odd field-item article'}).find_all('p')
+                for paragraph in paragraphs:
+                    text = paragraph.get_text().strip()
+                    if text is not None and text != '':
+                        pargs.append(text)
+            elif lang == 'milenio':
+                res = requests.get('https://www.milenio.com'+url)
+                soup = BeautifulSoup(res.text, 'html.parser')
+                title = soup.find('div', {'class': 'bg-content'})
+                tit = title.find('span').get_text()
+                paragraphs = soup.find('div', {'id': 'content-body'}).find_all(re.compile('^((p)|(blockquote)|(h2)|(li))$'))
+                for paragraph in paragraphs:
+                    text = paragraph.get_text().strip()
+                    if text is not None and len(text)>0:
+                        pargs.append(text)
+            if len(pargs)>0 and len(tit)>0:
+                rs = {
+                    'tit' : tit,
+                    'pargs' : pargs,
+                }
+                js = json.dumps(rs)
+                file = path.open(mode='w')
+                file.write(js)
+                file.close()
 
         print(tit)
         print()
